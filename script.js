4c9a5c7852f2721e951cf6cc3f779a55418f9649
Courtney McKown <wheelerzac@yahoo.com>
2023-04-24 18:33:28 -0500

    added api var and search event

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-var apiKey ="cf07720502e46b7205f0e6b5ce3e8393"
-var weatherUrl = 
-var forecastUrl =
+var apiKey ="&appid=cf07720502e46b7205f0e6b5ce3e8393";
+var baseUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+var fullUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+
+function handleCitySearch(event){
+//get value of input field
+//build a url by combining baseUrl + input value + api key    
+};
+
+searchBtn.addEventListener('click', handleCitySearch);
