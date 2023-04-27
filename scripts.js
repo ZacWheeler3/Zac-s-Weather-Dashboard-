9d848d6371a6a0d04b021e96eada860399c552c6
Courtney McKown <wheelerzac@yahoo.com>
2023-04-26 22:42:28 -0500

    added js code to pull api infor

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ var baseUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=';
 var fullUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
 var searchBtn = document.getElementById("search-button");
 
+console.log("hello World");
+
 function getWeatherResults(lat, lon) {
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=cf07720502e46b7205f0e6b5ce3e8393&units=imperial`;
     fetch(requestUrl)
@@ -11,27 +13,27 @@ function getWeatherResults(lat, lon) {
         })
         .then(function (data) {
             console.log(data);
-            var card = $("<div>").addClass("card").attr("style","width:20vw");
+            var card = $("<div>").addClass("card").attr("style", "width:20vw");
             var cardTitle = $("<h3>").addClass("card-title").text(data.name);
             var cardBody = $("<div>").addClass("card-body");
-            var tempEl = $("<p>").addClass("card-text").text("TEMP: " + data.main.temp.toFixed(0)+ "F")
+            var tempEl = $("<p>").addClass("card-text").text("TEMP: " + data.main.temp.toFixed(0) + "F")
             $(".weather").append(card.append(cardTitle, cardBody, tempEl))
-        //     <div class="row row-cols-1 row-cols-md-3 g-4">
-        // <div class="col">
-        //   <div class="card h-100">
-        //     <div class="card-body">
-        //       <h5 class="card-title">Card title</h5>
-        //       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        //     </div>
-        //   </div>
-        // </div>
+            //     <div class="row row-cols-1 row-cols-md-3 g-4">
+            // <div class="col">
+            //   <div class="card h-100">
+            //     <div class="card-body">
+            //       <h5 class="card-title">Card title</h5>
+            //       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            //     </div>
+            //   </div>
+            // </div>
         })
 }
 
 function handleCitySearch(event) {
-    var cityName = $(this).siblings(".city-value").value().trim();
+    var cityName = $(this).siblings(".city-value").val().trim();
     console.log(cityName);
-    var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=cf07720502e46b7205f0e6b5ce3e8393`;
+    var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=cf07720502e46b7205f0e6b5ce3e8393`;
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
